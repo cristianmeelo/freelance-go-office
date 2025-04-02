8b9b4c7a1a3eb4085d98cb5c75f1f9dc59547514
cristian da rosa melo <cristian_melo@sicredi.com.br>
2025-04-02 08:07:45 -0300

    fix: fixed hero mobile

--- freelance-go-office/js/hero-slider.js
+++ freelance-go-office/js/hero-slider.js
@@ -4,12 +4,9 @@ let index = 0;
 export function moveSlider() {
   index++;
   if (index >= slider.children.length) {
-    // Garante que não exceda o número de slides
     index = 0;
   }
   slider.style.transform = `translateX(-${index * 100}%)`;
 }
 
-// Define o intervalo para alternar os slides automaticamente
 setInterval(moveSlider, 3000);
-  
